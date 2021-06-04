3445088003e23bfc32b2243a6c420d1e20753d48
Tomas <tomas.v.mob@gmail.com>
2021-06-05 02:09:16 +0300

    ND0603 - Nr.4 Complete

--- ND0603/nd0603.php
+++ ND0603/nd0603.php
@@ -72,6 +72,7 @@ _d($chars);
 _d($masyvas2);
 
 echo '-------- -------- 04 -------- --------<br>';
+// 60 lines of program code :)
 $indexes_min_sorted = [];
 $values_min_sorted = [];
 foreach ($masyvas2 as $value_ii) {
@@ -103,10 +104,37 @@ foreach ($masyvas2 as $value_ii) {
     $indexes_min_sorted[] = $index_min_i;
     $values_min_sorted[] = $min_i;
 }
-_d($indexes_min_sorted);
-_d($values_min_sorted);
+// _d($indexes_min_sorted);
+// _d($values_min_sorted);
 foreach ($indexes_min_sorted as $index => $value) {
     $masyvas2Sorted[$index] = $masyvas2[$value];
 }
 $masyvas2 = $masyvas2Sorted;
+_d($masyvas2);
+$indexes_K_sorted = [];
+foreach ($masyvas2 as $ii => $value_ii) {
+    foreach ($value_ii as $value) {
+        if ($value === 'K') {
+            $indexes_K_sorted[] = $ii;
+            break;
+        }
+    }
+}
+foreach ($masyvas2 as $ii => $value_ii) {
+    $inList = false;
+    foreach ($indexes_K_sorted as $value) {
+        if ($ii === $value) {
+            $inList = true;
+            break;
+        }
+    }
+    if (!$inList) {
+        $indexes_K_sorted[] = $ii;
+    }
+}
+// _d($indexes_K_sorted);
+foreach ($indexes_K_sorted as $index => $value) {
+    $masyvas2_KSorted[$index] = $masyvas2[$value];
+}
+$masyvas2 = $masyvas2_KSorted;
 _d($masyvas2);
